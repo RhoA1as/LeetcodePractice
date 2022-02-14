e0d98403e5a6e82d7d6f218d0890bb21bbd5b5ec
RhoA1as <2731418851@qq.com>
2022-02-14 21:14:31 +0800

    [Practice]有序数组中的单一元素 [link]https://leetcode-cn.com/problems/single-element-in-a-sorted-array/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- src/main/java/com/oasis/LeetCode.java
+++ src/main/java/com/oasis/LeetCode.java
@@ -1730,6 +1730,20 @@ public class LeetCode {
         cnts[4] /= 2;
         return Arrays.stream(cnts).min().getAsInt();
     }
+
+    //https://leetcode-cn.com/problems/single-element-in-a-sorted-array/ 有序数组中的单一元素
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while(l < r){
+            int m = l + (r -l) / 2;
+            if(nums[m] == nums[m ^ 1]){
+                l = m + 1;
+            }else {
+                r = m;
+            }
+        }
+        return nums[l];
+    }
 }
 
 //https://leetcode-cn.com/problems/shuffle-an-array/ 打乱数组

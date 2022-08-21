737fa56c93d226ee8b4c89bf4fb1e9664c6ebfa7
RhoA1as <2731418851@qq.com>
2022-08-21 13:28:06 +0800

    [Practice]检查单词是否为句中其他单词的前缀 [link]https://leetcode.cn/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- src/main/java/com/oasis/bean/LeetcodeP2.java
+++ src/main/java/com/oasis/bean/LeetcodeP2.java
@@ -726,6 +726,18 @@ class MagicDictionary {
         }
         return ans;
     }
+
+    //https://leetcode.cn/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/ 检查单词是否为句中其他单词的前缀
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        if(sentence == null || searchWord == null){
+            return -1;
+        }
+        String[] s = sentence.split(" ");
+        for (int i = 0; i < s.length; i++) {
+            if(s[i].startsWith(searchWord)) return i + 1;
+        }
+        return -1;
+    }
 }
 
 class TreeNode {

40fdc8056753018856115cbdea157b03f39a752d
RhoA1as <2731418851@qq.com>
2022-10-08 13:02:08 +0800

    [Practice]优势洗牌 [link]https://leetcode.cn/problems/advantage-shuffle/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- src/main/java/com/oasis/bean/LeetcodeP2.java
+++ src/main/java/com/oasis/bean/LeetcodeP2.java
@@ -1057,6 +1057,30 @@ class MagicDictionary {
         }
         return max;
     }
+
+    //https://leetcode.cn/problems/advantage-shuffle/ 优势洗牌
+    public int[] advantageCount(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null) return null;
+        int n = nums1.length;
+        Integer[] idx1 = new Integer[n];
+        Integer[] idx2 = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            idx1[i] = i;
+            idx2[i] = i;
+        }
+        Arrays.sort(idx1, (x,y) -> nums1[x] - nums1[y]);
+        Arrays.sort(idx2, (x,y) -> nums2[x] - nums2[y]);
+        int left = 0, right = n - 1;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if(nums1[idx1[i]] > nums2[idx2[left]]){
+                ans[idx2[left++]] = nums1[idx1[i]];
+            } else {
+                ans[idx2[right--]] = nums1[idx1[i]];
+            }
+        }
+        return ans;
+    }
 }
 
 class TreeNode {

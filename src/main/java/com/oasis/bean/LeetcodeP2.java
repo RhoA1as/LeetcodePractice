c9853ae02b58e80ef7a14f37b1eddde0d113ce69
RhoA1as <2731418851@qq.com>
2023-02-15 01:39:51 +0800

    [Practice]表现良好的最长时间段 [link]https://leetcode.cn/problems/longest-well-performing-interval/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- src/main/java/com/oasis/bean/LeetcodeP2.java
+++ src/main/java/com/oasis/bean/LeetcodeP2.java
@@ -1357,6 +1357,24 @@ class MagicDictionary {
         if (root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
         return evaluateTree(root.left) && evaluateTree(root.right);
     }
+
+    //https://leetcode.cn/problems/longest-well-performing-interval/ 表现良好的最长时间段
+    public int longestWPI(int[] hours) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0, s = 0;
+        for (int i = 0; i < hours.length; i++) {
+            s += hours[i] > 8 ? 1 : -1;
+            if (s > 0) {
+                res = Math.max(res, i + 1);
+            } else if (map.containsKey(s - 1)){
+                res = Math.max(res, i - map.get(s - 1));
+            }
+            if (!map.containsKey(s)) {
+                map.put(s, i);
+            }
+        }
+        return res;
+    }
 }
 
 class ListNode {

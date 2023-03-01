2cc64f0dcf1b719898db3919d39641b2775f03a9
RhoA1as <2731418851@qq.com>
2023-03-01 13:44:05 +0800

    [Practice]矩阵中的局部最大值 [link]https://leetcode.cn/problems/largest-local-values-in-a-matrix/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- Leetcodepy/leetcode2.py
+++ Leetcodepy/leetcode2.py
@@ -1043,6 +1043,15 @@ class Solution:
             m[v] += w
         return sorted([[v, w] for v, w in m.items()])
 
+    # https://leetcode.cn/problems/largest-local-values-in-a-matrix/ 矩阵中的局部最大值
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        ans = [[0] * (n - 2) for _ in range(n - 2)]
+        for i in range(n - 2):
+            for j in range(n - 2):
+                ans[i][j] = max(grid[a][b] for a in range(i, i + 3) for b in range(j, j + 3))
+        return ans
+
 
 # https://leetcode.cn/problems/design-an-ordered-stream/ 设计有序流
 class OrderedStream:

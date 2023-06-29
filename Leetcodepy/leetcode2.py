342b31b01244866f93d3318fc0f7f9e5da9c6c21
RhoA1as <2731418851@qq.com>
2023-06-29 23:54:30 +0800

    [Practice]重构 2 行二进制矩阵 [link]https://leetcode.cn/problems/reconstruct-a-2-row-binary-matrix/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- Leetcodepy/leetcode2.py
+++ Leetcodepy/leetcode2.py
@@ -1733,6 +1733,31 @@ class Solution:
             res = max(dp0, dp1, res)
         return res
 
+    # https://leetcode.cn/problems/reconstruct-a-2-row-binary-matrix/ 重构 2 行二进制矩阵
+    def reconstructMatrix(self, upper: int, lower: int, colsum: List[int]) -> List[List[int]]:
+        sum_val = two_sum = 0
+        for c in colsum:
+            if c == 2:
+                two_sum += 1
+            sum_val += c
+        if sum_val != upper + lower or two_sum > min(upper, lower):
+            return []
+        n = len(colsum)
+        ans = [[0] * n for _ in range(2)]
+        upper -= two_sum
+        lower -= two_sum
+        for i in range(n):
+            if colsum[i] == 2:
+                ans[0][i] = ans[1][i] = 1
+            elif colsum[i] == 1:
+                if upper > 0:
+                    ans[0][i] = 1
+                    upper -= 1
+                else:
+                    ans[1][i] = 1
+                    lower -= 1
+        return ans
+
 
 # https://leetcode.cn/problems/design-an-ordered-stream/ 设计有序流
 class OrderedStream:

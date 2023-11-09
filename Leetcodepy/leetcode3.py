f452e69a45d41811011700139f1372c86fe148e1
RhoA1as <2731418851@qq.com>
2023-11-09 10:36:49 +0800

    [practice]最长平衡子字符串 [link]https://leetcode.cn/problems/find-the-longest-balanced-substring-of-a-binary-string/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- Leetcodepy/leetcode3.py
+++ Leetcodepy/leetcode3.py
@@ -24,3 +24,18 @@ def connect(self, root: 'Node') -> 'Node':
             if cur.right:
                 queue.append(cur.right)
     return root
+
+
+# https://leetcode.cn/problems/find-the-longest-balanced-substring-of-a-binary-string/ 最长平衡子字符串
+def findTheLongestBalancedSubstring(self, s: str) -> int:
+    res = cnt0 = cnt1 = 0
+    for i in range(len(s)):
+        if s[i] == "1":
+            cnt1 += 1
+            res = max(res, 2 * min(cnt0, cnt1))
+        elif i == 0 or s[i-1] == "1":
+            cnt0 = 1
+            cnt1 = 0
+        else:
+            cnt0 += 1
+    return res

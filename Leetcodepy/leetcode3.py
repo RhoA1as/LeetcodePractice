a28118d04f44494d8213d473d0208e870128cd29
RhoA1as <2731418851@qq.com>
2023-11-17 01:33:50 +0800

    [Practice]最长奇偶子数组 [link]https://leetcode.cn/problems/longest-even-odd-subarray-with-threshold/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- Leetcodepy/leetcode3.py
+++ Leetcodepy/leetcode3.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
@@ -33,9 +36,25 @@ def findTheLongestBalancedSubstring(self, s: str) -> int:
         if s[i] == "1":
             cnt1 += 1
             res = max(res, 2 * min(cnt0, cnt1))
-        elif i == 0 or s[i-1] == "1":
+        elif i == 0 or s[i - 1] == "1":
             cnt0 = 1
             cnt1 = 0
         else:
             cnt0 += 1
+    return res
+
+
+# https://leetcode.cn/problems/longest-even-odd-subarray-with-threshold/ 最长奇偶子数组
+def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+    res = i = 0
+    n = len(nums)
+    while i < n:
+        if nums[i] > threshold or nums[i] % 2:
+            i += 1
+            continue
+        start = i
+        i += 1
+        while i < n and nums[i] <= threshold and nums[i] % 2 != nums[i-1] % 2:
+            i += 1
+        res = max(res, i - start)
     return res

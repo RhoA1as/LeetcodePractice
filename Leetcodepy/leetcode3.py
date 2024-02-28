615c3463379ec8e3c63eff6faac3eebd01757f27
RhoA1as <2731418851@qq.com>
2024-02-29 01:11:28 +0800

    [Practice]使二叉树所有路径值相等的最小代价 [link]https://leetcode.cn/problems/make-costs-of-paths-equal-in-a-binary-tree/
    Signed-off-by: RhoA1as <2731418851@qq.com>

--- Leetcodepy/leetcode3.py
+++ Leetcodepy/leetcode3.py
@@ -75,3 +75,12 @@ def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
             i += 1
         res = max(res, i - start)
     return res
+
+
+# https://leetcode.cn/problems/make-costs-of-paths-equal-in-a-binary-tree/ 使二叉树所有路径值相等的最小代价
+def minIncrements(self, n: int, cost: List[int]) -> int:
+    ans = 0
+    for i in range(n - 2, 0, -2):
+        ans += abs(cost[i] - cost[i + 1])
+        cost[i // 2] += max(cost[i], cost[i + 1])
+    return ans
